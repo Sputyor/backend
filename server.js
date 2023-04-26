78c4cba9a2098cfc15bfd44a9e12822131ce2d13
HalaszPeter1 <halasz.peter1@students.jedlik.eu>
2023-04-26 08:28:54 +0200

    asd

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 
 
 const corsOptions = {
-    origin:["https://sore-blue-elephant-toga.cyclic.app", "https://visionary-croquembouche-50b9b6.netlify.app"] ,
+    origin:'*' ,
     credentials: true,
 }
 
